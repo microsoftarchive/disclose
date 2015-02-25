446db1e8ffffb18c24f404628047efe03a5e54ef
myobie <me@nathanherald.com>
2015-02-25 21:45:33 +0100

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,11 @@
+# Disclose
+
+A small module to help toggle details on click in the dom.
+
+## Usage
+
+```js
+var disclose = require("disclose");
+var el document.querySelector("[data-disclose]");
+disclose(el);
+```
---
+++ index.js
@@ -0,0 +1,30 @@
+var Disclosure = function(el) {
+  var self = this;
+  self.el = el;
+  self.isActive = false;
+  self.details = el.querySelectorAll('[data-details]');
+  el.addEventListener('click', function(e) { self.toggle(e) });
+  self.hide();
+};
+
+Disclosure.prototype.hide = function() {
+  for (var i = 0; i < this.details.length; i++) {
+    this.details[i].style.display = 'none';
+  }
+};
+
+Disclosure.prototype.show = function() {
+  for (var i = 0; i < el.details.length; i++) {
+    this.details[i].style.display = 'block';
+  }
+};
+
+Disclosure.prototype.toggle = function(e) {
+  e.stopPropagation();
+  this.isActive = !this.isActive;
+  if (this.isActive) { this.show() } else { this.hide() }
+};
+
+module.exports = function(el) {
+  return new Disclosure(el);
+};
